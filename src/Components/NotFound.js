85a71bd1000e224d46e9057897d8c4534f9bfbff
amandaneal1131 <amandaneal1131@gmail.com>
2020-12-21 12:59:15 -0600

    changed html

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -5,7 +5,7 @@ class NotFound extends React.Component{
     render(){
         return(
         <div className='notFound'>
-            <h2>404 - Page not found</h2>
+            <h2>404 - Page not found.</h2>
             <p>Try going back to your previous page.</p>
         </div>
         )
